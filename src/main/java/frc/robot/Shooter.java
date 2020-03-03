0dc9540db062a829300c1fd0b005630c42c5959d
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-03-02 19:56:08 -0800

    in progress
    Co-Authored-By: sparklefbanks <sparklefbanks@users.noreply.github.com>

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -12,24 +12,52 @@ public class Shooter extends AgencySystem{
     protected Boolean shotRequested = false;
     public enum ShooterAngle{SHORT,LONG};
 
+    private CANSparkMax m_motor1;
+    private CANSparkMax m_motor2;
+    private CANSparkMax m_angle;
 
-    public Shooter(int deviceID1, int deviceID2, int deviceID3, String name,
+
+    public Shooter(int deviceID1, int deviceID2, int angleID, String name,
             Boolean debug) {
 
         this.name = name;
         this.debug = debug;
 
+        m_motor1 = new CANSparkMax(deviceID1, MotorType.kBrushless);
+        m_motor2 = new CANSparkMax(deviceID2, MotorType.kBrushless);
+        m_angle = new CANSparkMax(angleID, MotorType.kBrushless);
+
+        m_motor1.restoreFactoryDefaults();
+        m_motor2.restoreFactoryDefaults();
+        m_angle.restoreFactoryDefaults();
+
     }
 
     public void requestShot() {
 
+        this.shotRequested = true;
+
+    }
+
+    public void requestStop() {
+        this.shotRequested = false;
     }
 
     public void requestAngleChange(ShooterAngle requestedAngle) {
 
+        //???? por que = requestedAngle;
+
     }
 
     public void teleopPeriodic() {
+        if (shotRequested) {
+            m_motor1.set(.65);
+            m_motor2.set(.65);
+        }
+        else {
+            m_motor1.set(0);
+            m_motor2.set(0);
+        }
 
     }
 
