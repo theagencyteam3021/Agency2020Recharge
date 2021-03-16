c5e9c141e838b3b9982ebc2ce944bfc2bad33525
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2021-03-15 19:46:42 -0700

    Add basic turning logic (not tested)

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -4,6 +4,8 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import java.lang.Math;
+
 public class Autonomous extends AgencySystem {
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("vision");
@@ -13,11 +15,21 @@ public class Autonomous extends AgencySystem {
     NetworkTableEntry by = table.getEntry("by");
     NetworkTableEntry bv = table.getEntry("bv");
 
+    private double sigmoid(double input, double stopPoint, double roughness) {
+        double result = 2/(Math.pow((Math.E),(roughness*((-1.0*input)+stopPoint))))-1;
+        return result;
+    }
+
     public void teleopPeriodic() {
         double w = bw.getDouble(0.0);
         double h = bh.getDouble(0.0);
         double x = bx.getDouble(0.0);
         double y = by.getDouble(0.0);
         boolean v = bv.getBoolean(false);
+
+        double turnPower;
+        if(v) {
+            turnPower = sigmoid(x, 0.5, 40.0); //Change third paramater to negative if it turns opposite
+        }
     }
 }
